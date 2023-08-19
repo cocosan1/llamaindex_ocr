30c0c0ac527805e50ad160b3403d7d8ce04ef5b3
hskw1 <isishskw@gmail.com>
2023-08-20 07:50:31 +0900

    16th commit

--- main.py
+++ main.py
@@ -66,25 +66,25 @@ st.code(num)
 options = Options() 
 options.add_argument("--headless=new")
 
-cmd = 'pip install --upgrade chromedriver_binary' 
-res = subprocess.call(cmd, shell=True) #True 文字列で指定 False リストで指定
+# cmd = 'pip install --upgrade chromedriver_binary' 
+# res = subprocess.call(cmd, shell=True) #True 文字列で指定 False リストで指定
 
-# chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications" : 2} #通知ポップアップを無効
-options.add_experimental_option("prefs",prefs)
-options.add_argument('--disable-gpu')
-# chrome_options.add_argument('--headless')
-options.add_argument("--no-sandbox")
-#セキュリティのためにサンドボックス環境内で実行される。
-# 一部の状況ではこのサンドボックス機能が問題を引き起こすことがあります
-options.add_argument("--disable-dev-shm-usage")
-#ブラウザが共有メモリを使用しないように設定します。代わりに一般的なRAMを使用するため、一部の制約を回避する
-options.add_argument("--disable-features=NetworkService")
-#一部の状況や環境でNetwork Serviceが正しく機能しないことがあり、ブラウザの動作に問題を引き起こす可能性があります。
-options.add_argument("--window-size=1920x1080")
-options.add_argument("--disable-features=VizDisplayCompositor")
-#ウェブページの描画を最適化して滑らかなスクロールやアニメーションを実現.
-# スクレイピングの場合、この機能が望ましくない場合がある
+# # chrome_options = webdriver.ChromeOptions()
+# prefs = {"profile.default_content_setting_values.notifications" : 2} #通知ポップアップを無効
+# options.add_experimental_option("prefs",prefs)
+# options.add_argument('--disable-gpu')
+# # chrome_options.add_argument('--headless')
+# options.add_argument("--no-sandbox")
+# #セキュリティのためにサンドボックス環境内で実行される。
+# # 一部の状況ではこのサンドボックス機能が問題を引き起こすことがあります
+# options.add_argument("--disable-dev-shm-usage")
+# #ブラウザが共有メモリを使用しないように設定します。代わりに一般的なRAMを使用するため、一部の制約を回避する
+# options.add_argument("--disable-features=NetworkService")
+# #一部の状況や環境でNetwork Serviceが正しく機能しないことがあり、ブラウザの動作に問題を引き起こす可能性があります。
+# options.add_argument("--window-size=1920x1080")
+# options.add_argument("--disable-features=VizDisplayCompositor")
+# #ウェブページの描画を最適化して滑らかなスクロールやアニメーションを実現.
+# # スクレイピングの場合、この機能が望ましくない場合がある
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install(), options=options))
 
